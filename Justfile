5bf2d6e9311c7d386143e0fd5bcd832bd87e66f9
mxxntype <59417007+mxxntype@users.noreply.github.com>
2025-02-16 01:01:29 +0300

    feat: Add a Nix devshell and a `Justfile`

---
+++ Justfile
@@ -0,0 +1,26 @@
+# List all available recipes.
+default:
+    @docker compose ps
+
+# Fire up all services.
+start:
+    docker compose up --detach
+    docker compose logs --follow
+
+# Bring all services to a halt.
+stop:
+    docker compose down
+
+# Shutdown and re-ignite all services.
+restart: stop start
+
+# Ensure correct ownership of the `./data` folder.
+[confirm]
+set-ownership:
+    sudo chown astrumaureus:users -R data
+
+# Erase all services' persistent data volumes.
+[confirm]
+flushall: stop set-ownership
+    docker compose down --volumes
+    rm -rf ./data/postgresql/*
---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        inherit (pkgs) stdenv lib;
+
+        buildInputs = with pkgs; [ openssl postgresql_17.lib ];
+        nativeBuildInputs = with pkgs; [ redis mongosh neo4j postgresql_17 ];
+        envVars = { };
+
+      in {
+        devShells = let
+          shellOverrides = if stdenv.isLinux then {
+            stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.clangStdenv;
+          } else
+            { };
+        in {
+          default = pkgs.mkShell.override shellOverrides (envVars // {
+            inherit buildInputs nativeBuildInputs;
+            shellHook = ''
+              # This hellish thing is needed to correctly *append* to the
+              # system's $LD_LIBRARY_PATH instead of overwriting it (which
+              # may cause external software to break).
+              export LD_LIBRARY_PATH="''${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}${
+                lib.makeLibraryPath buildInputs
+              }"
+            '';
+          });
+        };
+
+        formatter = pkgs.nixfmt-classic;
+      });
+}
